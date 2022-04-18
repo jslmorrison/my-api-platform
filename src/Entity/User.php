6cf36b14f8325288d5f7ff4bf02cbb0fa386ea76
John Morrison <jslmorrison@gmail.com>
2022-04-18 15:05:15 +0100

    Add doctrine mapping to entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,14 +4,28 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
 
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`users`')]
 class User
 {
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\GeneratedValue(strategy: 'NONE')]
     private Uuid $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
     private string $name;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false, unique: true)]
     private string $email;
+
+    #[ORM\Column(type: 'datetime', nullable: false)]
     private \DateTimeInterface $createdAt;
+
+    #[ORM\Column(type: 'boolean', nullable: false)]
     private bool $enabled = false;
 
     private function __construct()

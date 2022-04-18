90cda9a0640c37c5451b8e5a5729d20b5238c6b3
John Morrison <jslmorrison@gmail.com>
2022-04-18 15:20:39 +0100

    Add user fixture data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,16 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
-class AppFixtures extends Fixture
+final class AppFixtures extends Fixture
 {
+    private $faker;
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 100; $i++) {
+            $user = User::namedWithEmail($faker->name, $faker->safeEmail);
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }

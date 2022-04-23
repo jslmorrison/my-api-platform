cea4d6f70b9093459ace81e5efbe88b06d28aba5
John Morrison <jslmorrison@gmail.com>
2022-04-23 23:02:34 +0100

    Add orm mapping

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ProductDoctrineRepository;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
 
+#[ORM\Entity(repositoryClass: ProductDoctrineRepository::class)]
+#[ORM\Table(name: '`products`')]
+#[ApiResource()]
 class Product
 {
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\GeneratedValue(strategy: 'NONE')]
+    private Uuid $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
     private string $name;
 
     private function __construct()

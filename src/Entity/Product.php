3d4f78feca9cd69ed991477abed58ed0029e9dba
John Morrison <jslmorrison@gmail.com>
2022-04-24 11:52:21 +0100

    Move orm and api platform mappings to config files
    Keeps the entity classes clean.
    Attributes are OK, better than annotations, but eventually fill the class with 'noise'.

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,24 +4,11 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\DTO\ProductInput;
-use App\DTO\ProductOutput;
-use App\Repository\ProductDoctrineRepository;
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
 
-#[ORM\Entity(repositoryClass: ProductDoctrineRepository::class)]
-#[ORM\Table(name: '`products`')]
-#[ApiResource(input: ProductInput::class, output: ProductOutput::class)]
 class Product
 {
-    #[ORM\Id]
-    #[ORM\Column(type: 'uuid', unique: true)]
-    #[ORM\GeneratedValue(strategy: 'NONE')]
     private Uuid $id;
-
-    #[ORM\Column(type: 'string', length: 255, nullable: false)]
     private string $name;
 
     private function __construct()
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,33 +4,14 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use App\DTO\UserInput;
-use App\DTO\UserOutput;
-use App\Repository\UserDoctrineRepository;
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
 
-#[ORM\Entity(repositoryClass: UserDoctrineRepository::class)]
-#[ORM\Table(name: '`users`')]
-#[ApiResource(input: UserInput::class, output: UserOutput::class)]
 class User
 {
-    #[ORM\Id]
-    #[ORM\Column(type: 'uuid', unique: true)]
-    #[ORM\GeneratedValue(strategy: 'NONE')]
     private Uuid $id;
-
-    #[ORM\Column(type: 'string', length: 255, nullable: false)]
     private string $name;
-
-    #[ORM\Column(type: 'string', length: 255, nullable: false, unique: true)]
     private string $email;
-
-    #[ORM\Column(type: 'datetime', nullable: false)]
     private \DateTimeInterface $createdAt;
-
-    #[ORM\Column(type: 'boolean', nullable: false)]
     private bool $enabled = false;
 
     private function __construct()
